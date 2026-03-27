0942f89276205a3bd3acf0b88916361fe66d4d07
Lunarian-Nova <ckeister@anamosa.k12.ia.us>
2026-03-27 15:19:30 +0000

    lmao

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+let counter = 100;
+let prefixIncrement = ++counter;
+let postfixIncrement = counter++;
+let prefixDecrement = --counter;
+let postfixDecrement = counter--;
 
+document.getElementById('result').innerHTML = counter;
+
+// Export for the test environment (puppeteer page.evaluate() can access global vars)
+window.counter = counter;
+window.prefixIncrement = prefixIncrement;
+window.postfixIncrement = postfixIncrement;
+window.prefixDecrement = prefixDecrement;
+window.postfixDecrement = postfixDecrement;
